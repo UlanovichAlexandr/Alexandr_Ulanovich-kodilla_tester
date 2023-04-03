4e15ab12ad5a54b01b67b7ff1f4ccc4d5240456e
UlanovichAlexaandr <ulanovichalexandr@gazeta.pl>
2023-04-03 20:26:58 +0200

    modul 7

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Aliaksandr");
+        System.out.println("30");
+        System.out.println("U");
+        }
+    }
